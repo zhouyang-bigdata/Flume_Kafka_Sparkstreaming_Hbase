3ae2e5d1807672b4d4081cfc56346e0b239facb3
zhouyangzy1@outlook.com <53464334ads>
2019-03-06 16:25:43 +0800

    增加hbase工具类，

---
+++ hbase/src/main/java/com.hbase/main/HbaseOperate.java
@@ -0,0 +1,44 @@
+package com.hbase.main;
+
+import com.alibaba.fastjson.JSONArray;
+import com.hbase.hbaseUtils.HBaseQueryUtils2;
+import com.hbase.hbaseUtils.HBaseQueryUtils3;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @ClassName HbaseOperate
+ * @Description TODO
+ * @Author zhouyang
+ * @Date 2019/3/6 11:14
+ * @Version 1.0
+ **/
+public class HbaseOperate {
+    public static void main(String[] args){
+        List<Map<String, Object>> dataList = new ArrayList<Map<String, Object>>();
+        Map<String, Object> map1 = new HashMap<>();
+        Map<String, Object> map2 = new HashMap<>();
+        Map<String, Object> map3 = new HashMap<>();
+
+        map1.put("id", "S0_002904_1045_1");
+        map2.put("id", "S0_002904_1045_2");
+        map3.put("id", "S0_002904_1045_3");
+        //
+        dataList.add(map1);
+        dataList.add(map2);
+        dataList.add(map3);
+
+        //获取到了rowkey,从hbase中查出数据
+        JSONArray jsonArray = new JSONArray();
+        HBaseQueryUtils2 hBaseQueryUtils2 = new HBaseQueryUtils2();
+        try {
+            jsonArray = hBaseQueryUtils2.getDataListByRowKeyList(dataList);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println(jsonArray.toJSONString());
+    }
+}
